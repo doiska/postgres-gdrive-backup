cef2f600eff6626e5df5e0b7a9c665aa5a494a81
DIBASHI, John <johndibashi2001@gmail.com>
2024-06-09 03:47:07 +0100

    Add Automated Stale Backup Deletion (#2)
    * feat: add retention to database backup
    
    * chore: remove dotenv dependency
    
    ---------
    
    Co-authored-by: Kyng Cytro <johndibashi2001@gamil.com>

--- src/env.ts
+++ src/env.ts
@@ -1,21 +1,25 @@
-import { cleanEnv, str, json, bool } from 'envalid';
+import { cleanEnv, str, json, bool } from "envalid";
 
 export const env = cleanEnv(process.env, {
-    SERVICE_ACCOUNT: json<{
-        type: string;
-        project_id: string;
-        private_key_id: string;
-        private_key: string;
-        client_email: string;
-        client_id: string;
-        auth_uri: string;
-        token_uri: string;
-        auth_provider_x509_cert_url: string;
-        client_x509_cert_url: string;
-    }>(),
-    FILE_PREFIX: str({ default: 'db-backup-' }),
-    DATABASE_URL: str(),
-    FOLDER_ID: str(),
-    CRON_EXPRESSION: str({ default: '0 0 * * *' }),
-    RUN_ON_START: bool({ default: true })
+  SERVICE_ACCOUNT: json<{
+    type: string;
+    project_id: string;
+    private_key_id: string;
+    private_key: string;
+    client_email: string;
+    client_id: string;
+    auth_uri: string;
+    token_uri: string;
+    auth_provider_x509_cert_url: string;
+    client_x509_cert_url: string;
+  }>(),
+  FILE_PREFIX: str({ default: "db-backup-" }),
+  RETENTION: str({
+    choices: ["week", "month", "year", "disabled"],
+    default: "disabled",
+  }),
+  DATABASE_URL: str(),
+  FOLDER_ID: str(),
+  CRON_EXPRESSION: str({ default: "0 0 * * *" }),
+  RUN_ON_START: bool({ default: true }),
 });
